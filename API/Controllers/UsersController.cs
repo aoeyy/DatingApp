131a65a4859d4bb8fc0055b9cffe5d44265920ed
Ashley Sun <ashley.sun@uoit.net>
2022-06-02 01:35:06 -0400

    End of Section 7 and 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,32 +8,40 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
 
 namespace API.Controllers
 {
-
-
+    [Authorize]
     public class UsersController : BaseAPIController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        //[AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            var users = await _userRepository.GetUsersAsync();
+
+            //var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+
+            return Ok(users);
         }
 
         //add an authorize attribute to ensure that our enpoints are protected with authentication
-        [Authorize]    
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        //[Authorize]    
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetMemberAsync(username);
         }
 
     }
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+
+            //checking if their birthday passed yet
+            if (dob.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
